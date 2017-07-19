48b7b2b9a45a239216ef17dee1f2d0d5c2678d86
drewbilee <drewbilee@puppetlabs.vm>
2017-07-19 08:10:49 -0700

    lab 10.1 - resource defaults

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,28 +1,24 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
   package { 'nginx':
     ensure => latest,
   }
   file { 'docroot':
     ensure => directory,
     path   => '/var/www',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
   file { 'index.html':
     ensure => file,
     path   => '/var/www/index.html',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/nginx/index.html',
   }
   file { 'nginx.conf':
     ensure  => file,
     path    => '/etc/nginx/nginx.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
