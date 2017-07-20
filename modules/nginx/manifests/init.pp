31926f67762b80b2f082e719ee23ab922f4819ff
drewbilee <drewbilee@puppetlabs.vm>
2017-07-20 06:03:27 -0700

    lab 13.1 - parameterization

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,10 +1,13 @@
-class nginx {
+class nginx (
+  Optional[String] $root = undef,
+  Boolean $highperf = true,
+) {
   case $facts['os']['family'] {
     'RedHat', 'Debian': {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $docroot = '/var/www'
+      $default_docroot = '/var/www'
       $confdir = '/etc/nginx'
       $blockdir = '/etc/nginx/conf.d'
       $logdir = '/var/log/nginx'
@@ -14,12 +17,16 @@ class nginx {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      $default_docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx/conf'
       $blockdir = 'C:/ProgramData/nginx/conf.d'
       $logdir = 'C:/ProgramData/nginx/logs'
       $service = 'nginx'
     }
+  }
+  $docroot = $root ? {
+    undef   => $default_docroot,
+    default => $root,
   }
   $user = $facts['os']['family'] ? {
     'windows' => 'nobody',
@@ -45,6 +52,7 @@ class nginx {
       confdir  => $confdir,
       blockdir => $blockdir,
       logdir   => $logdir,
+      highperf => $highperf,
     }),
     require => Package[$package],
   }
