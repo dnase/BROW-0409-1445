6fda5e3d5ad52be0a7eaa74ac760c436bb77ac1a
drewbilee <drewbilee@puppetlabs.vm>
2017-07-20 10:32:39 -0700

    capstone lab attempt #2

--- modules/profile/manifests/wordpress.pp
+++ modules/profile/manifests/wordpress.pp
@@ -1,10 +1,16 @@
 class profile::wordpress {
-  wordpress_app::simple { 'capstone':
-    nodes                           => {
-      Node['drewbilee.puppetlabs.vm'] => [
-        Wordpress_app::Database['capstone'],
-        Wordpress_app::Web['capstone'],
-      ]
-    }
+  class { 'mysql::server':
+    root_password => 'puppetlabs',
   }
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
+  include apache
+  include apache::mod::php
+  apache::vhost { $::fqdn:
+    port     => '80',
+    priority => '00',
+    docroot  => '/opt/wordpress',
+  }
+  include ::wordpress
 }
