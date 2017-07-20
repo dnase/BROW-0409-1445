c59e2dcd9c24c5669c5b18418f1561c2c3efe5cb
drewbilee <drewbilee@puppetlabs.vm>
2017-07-20 10:11:05 -0700

    updated site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,6 @@
 Host <<| tag == 'puppetlabs' |>>
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
   include role::classroom
   include profile::epel
 }
